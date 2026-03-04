5858d107ed817ec376619e5f6821ebad8d54280e
Natalia <natalia@MacBook-Air-Natalia.local>
2026-03-04 18:14:45 +0100

    start

---
+++ src/usadel_core.jl
@@ -0,0 +1,37 @@
+#Usadel core
+
+#Definicja parametrów
+struct params
+    E::Float64
+    Γin::Float64 
+    D::Vector{Float64}
+    Δ::Vector{Float64}
+    dx::Float64
+    N::Int
+end 
+
+#Parametry symulacji
+Ln, Ls =200,100
+dx=1.0
+nn=Int(Ln/dx)
+ns=Int(Ls/dx)
+N=(Ln+Ls)/dx
+
+#Inicjalizacja słownika
+nodes=Dict{Int,Dict{Symbol,Any}}()
+for n in 1:N
+    nodes[n]=Dict()
+    if n==1
+        nodes[n][:type]=:vacc
+    elseif n<nn
+        nodes[n][:type]=:N
+    elseif n==nn
+        nodes[n][:type]=:NS
+    elseif n==nn+1
+        nodes[n][:type]=:SN
+    elseif n>nn+1 && n<N
+        nodes[n][:type]=:S
+    else
+        nodes[n][:type]=:vacc
+    end
+end
