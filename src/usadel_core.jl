fc96572aaaf9949f72ce8043c1f507e0cf5d1d71
Natalia <natalia@MacBook-Air-Natalia.local>
2026-03-11 16:02:27 +0100

    tablice

--- src/usadel_core.jl
+++ src/usadel_core.jl
@@ -1,13 +1,14 @@
 #Usadel core
-
+#inicjalizacja słownika i tablic mozna zrobić w jednej pętli, ale dla przejrzystości zostawiłam to narazie w dwóch osobnych
 #Definicja parametrów
 struct params
     E::Float64
     Γin::Float64 
     D::Vector{Float64}
-    Δ::Vector{Float64}
+    Δ::Vector{ComplexF64}
     dx::Float64
     N::Int
+    nodes::Dict{Int,Symbol}
 end 
 
 #Parametry symulacji
@@ -17,21 +18,40 @@ nn=Int(Ln/dx)
 ns=Int(Ls/dx)
 N=Int((Ln+Ls)/dx)
 
-nodes=Dict{Int,Dict{Symbol,Any}}()
 #Inicjalizacja słownika
+node_map=Dict{Int,Symbol}()
 for n in 1:N
-    nodes[n]=Dict()
     if n==1
-        nodes[n][:type]=:vacc
+        node_map[n]=:vacc
     elseif n<nn
-        nodes[n][:type]=:N
+        node_map[n]=:N
     elseif n==nn
-        nodes[n][:type]=:NS
+        node_map[n]=:NS
     elseif n==nn+1
-        nodes[n][:type]=:SN
+        node_map[n]=:SN
     elseif n>nn+1 && n<N
-        nodes[n][:type]=:S
+        node_map[n]=:S
     else
-        nodes[n][:type]=:vacc
+        node_map[n]=:bulk
     end
 end
+
+#Inicjalizacja tablic
+D_tab=zeros(N)
+Δ_tab=zeros(ComplexF64,N)
+
+for n in 1:N
+    type=node_map[n]
+    if type==:vacc || type==:N || type==:NS
+        D_tab[n]=0.0152
+        Δ_tab[n]=0.0
+    elseif type==:SN || type==:S || type==:bulk
+        D_tab[n]=0.00636
+        Δ_tab[n]=1.0
+    end
+end
+
+#sprawdzenie
+println(params(0.8,12-3,D_tab,Δ_tab,dx,N,node_map))
+println(node_map[256])
+println(D_tab[256],Δ_tab[256])
